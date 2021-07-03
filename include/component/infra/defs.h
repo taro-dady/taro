b62808c35682f252cca8f475e51a632fff821fa1
fangtao <fangtao@qq.com>
2021-07-03 10:37:37 +0800

    add new version

---
+++ include/component/infra/defs.h
@@ -0,0 +1,9 @@
+﻿
+#pragma once
+
+#include "base/base.h"
+
+#define NAMESPACE_TARO_INFRA_BEGIN namespace taro { namespace infra {
+#define NAMESPACE_TARO_INFRA_END } }
+
+#define USING_NAMESPACE_TARO_INFRA using namespace taro::infra;
---
+++ test/api_test/src/main.cpp
@@ -0,0 +1,10 @@
+﻿
+#include "gtest/gtest.h"
+
+int main( int argc, char** argv )
+{
+    testing::InitGoogleTest( &argc, argv );
+    //testing::GTEST_FLAG( filter ) = "net_udp_group_test.*";
+    auto ret = RUN_ALL_TESTS();
+    return ret;
+}
